7c7e8adc04a13b6348934b920c0ec2c6ae9108e5
Lucosec <lucas564lith@gmail.com>
2024-10-04 19:01:07 -0300

    first commit

--- build.sh
+++ build.sh
@@ -12,8 +12,9 @@ RELEASE="$(rpm -E %fedora)"
 # List of rpmfusion packages can be found here:
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
-# this installs a package from fedora repos
-rpm-ostree install screen
+# this installs the cosmic desktop environment
+copr enable ryanabx/cosmic-epoch
+rpmos-tree install cosmic-epoch
 
 # this would install a package from rpmfusion
 # rpm-ostree install vlc
